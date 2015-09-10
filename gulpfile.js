c2deb05e14316f73055f265ca1063bd83c8be177
Jack Guy <jack@techwiq.com>
2015-09-10 14:31:50 -0500

    Fixed an issue where the gulp build wouldn't zip all files, simplified manifest file

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var zip = require('gulp-zip');
 var p = require('./package.json')
 
 gulp.task('default', function () {
-   return gulp.src('app/*')
+   return gulp.src('app/**/*')
       .pipe(zip('build'+p.version+'.zip'))
       .pipe(gulp.dest('builds'));
 });
